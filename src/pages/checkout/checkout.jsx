a117d1c525704dcbe8f834df4e72ff211a26eca8
Nicolò Migliore <nicolo.migliore@itiscuneo.eu>
2024-05-10 08:46:27 +0200

    css

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './checkout.css';
+import Successo from "./successo.png";
 
 export const Checkout = () => {
   return (
@@ -7,6 +8,7 @@ export const Checkout = () => {
         <div className="checkout-container">
             <h2>Ordine completato con successo!</h2>
             <p>Grazie per aver scelto i nostri prodotti. Il tuo pacchetto è stato preparato con cura e sarà spedito al più presto. Se hai ulteriori domande o richieste, non esitare a contattarci.</p>
+            <img src={Successo} alt="successo"></img>
         </div>
     </div>
   );
